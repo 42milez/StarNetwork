8b38524382dd34301d85640eda7a3b065d1399b2
Akihiro TAKASE <42milez@gmail.com>
2018-10-07 21:47:56 +0900

    Add signal handler

--- life/main.cpp
+++ life/main.cpp
@@ -1,7 +1,44 @@
+#include <csignal>
 #include <iostream>
 
-int main()
+namespace
 {
-  std::cout << "Hello, World!" << std::endl;
-  return 0;
+  class ExitHandler {
+  public:
+    static void exit() { should_exit_ = true; }
+    bool should_exit() const { return should_exit_; }
+  private:
+    static bool should_exit_;
+  };
+
+  bool ExitHandler::should_exit_ = false;
+
+  template <typename Func>
+  void register_handler(int signum, Func handler) {
+    struct sigaction act { nullptr };
+    act.sa_handler = handler;
+    act.sa_flags = 0;
+
+    sigset_t set;
+    sigemptyset(&set);
+    sigaddset(&set, signum);
+
+    act.sa_mask = set;
+
+    sigaction(signum, nullptr, &act);
+  }
+} // namespace
+
+int main(int argc, char** argv)
+{
+  // Handle Exit Signal
+  auto sig_handler = [](int signum) { ExitHandler::exit(); };
+  register_handler(SIGABRT, sig_handler);
+  register_handler(SIGINT, sig_handler);
+  register_handler(SIGTERM, sig_handler);
+
+  // Handle Pipe Signal
+  register_handler(SIGPIPE, SIG_IGN);
+  
+  return EXIT_SUCCESS;
 }
