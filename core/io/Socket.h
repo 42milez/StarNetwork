a7971b8dabbe1082859e061d713e51b374c9e032
Akihiro TAKASE <42milez@gmail.com>
2019-01-21 20:42:32 +0900

    Add Socket class

---
+++ core/io/Socket.h
@@ -0,0 +1,51 @@
+#ifndef P2P_TECHDEMO_CORE_IO_SOCKET_H
+#define P2P_TECHDEMO_CORE_IO_SOCKET_H
+
+#include <memory>
+
+namespace core
+{
+  namespace io
+  {
+    class Socket {
+    protected:
+      static Socket *(*_create)();
+
+    public:
+      static Socket *create();
+
+      enum class PollType {
+        POLL_TYPE_IN,
+        POLL_TYPE_OUT,
+        POLL_TYPE_IN_OUT
+      };
+
+      enum class Type {
+        TYPE_NONE,
+        TYPE_TCP,
+        TYPE_UDP
+      };
+
+      virtual Error open(Type p_type, IP::Type ip_type) = 0;
+      virtual void close() = 0;
+      virtual Error bind(IpAddress &ip, uint16_t port);
+      virtual Error listen(int max_pending) = 0;
+      virtual Error connect_to_host(IpAddress &ip, uint16_t port) = 0;
+      virtual Error poll(PollType type, int timeout) = 0;
+      virtual Error recv(uint8_t &buffer, int len, int &read_byte_count) = 0;
+      virtual Error recvfrom(uint8_t &buffer, int len, int &read_byte_count) = 0;
+      virtual Error send(const uint8_t &buffer, int len, int send_byte_count) = 0;
+      virtual Error sendto(const uint8_t &buffer, int len, int send_byte_count) = 0;
+      virtual std::shared_ptr<Socket> accept(IpAddress &ip, uint16_t port) = 0;
+
+      virtual bool is_open() const = 0;
+      virtual int get_available_bytes() const = 0;
+      virtual void set_broadcasting_enabled(bool enabled) = 0;
+      virtual void set_blocking_enabled(bool enabled) = 0;
+      virtual void set_tcp_no_delay_enabled(bool enabled) = 0;
+      virtual void set_reuse_address_enabled(bool enabled) = 0;
+    };
+  }
+}
+
+#endif // P2P_TECHDEMO_CORE_IO_SOCKET_H
