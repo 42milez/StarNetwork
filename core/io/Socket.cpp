009e66712b0b389e77429e99d2b29ccc353bffc8
Akihiro TAKASE <42milez@gmail.com>
2019-01-21 22:22:34 +0900

    Update Socket.cpp

--- core/io/Socket.cpp
+++ core/io/Socket.cpp
@@ -1,1 +1,23 @@
 #include "Socket.h"
+
+namespace core
+{
+  namespace io
+  {
+    Socket *
+    (*Socket::_create)() = nullptr;
+
+    Socket *
+    Socket::create()
+    {
+      if (_create)
+      {
+        return _create();
+      }
+
+      ERR_PRINT("Unable to create network socket, platform not supported");
+
+      reutnr nullptr;
+    }
+  } // namespace io
+} // namespace core
