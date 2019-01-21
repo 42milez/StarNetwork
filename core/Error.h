263a48ad9d79fab57d14ac59754773e4d3a7fffc
Akihiro TAKASE <42milez@gmail.com>
2019-01-21 20:42:46 +0900

    Add Error class

---
+++ core/Error.h
@@ -0,0 +1,12 @@
+#ifndef P2P_TECHDEMO_CORE_ERROR_H
+#define P2P_TECHDEMO_CORE_ERROR_H
+
+namespace core
+{
+  enum class Error {
+    OK,
+    FAILED
+  };
+} // namespace core
+
+#endif // P2P_TECHDEMO_CORE_ERROR_H
