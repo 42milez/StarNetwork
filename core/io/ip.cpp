8ebc4cf4c8dc361aabb138ad0ff7f0a541a4e044
Akihiro TAKASE <42milez@gmail.com>
2019-01-28 16:27:00 +0900

    update

--- core/io/ip.cpp
+++ core/io/ip.cpp
@@ -1,16 +1,19 @@
+#include <map>
 #include <mutex>
+
+#include "core/base/singleton.h"
 
 #include "ip.h"
 
 namespace core { namespace io
 {
-  struct _IP_ResolverPrivate
+  struct _IpResolverPrivate
   {
     struct QueueItem
     {
       volatile IP::ResolverStatus status;
 
-      IP_Address response;
+      IpAddress response;
 
       std::string hostname;
 
@@ -19,9 +22,9 @@ namespace core { namespace io
       void
       clear()
       {
-        status = IP::ResolverStatus::RESOLVER_STATUS_DONE;
-        response = IP_Address();
-        type = IP::Type::TYPE_NONE;
+        status = IP::ResolverStatus::DONE;
+        response = IpAddress();
+        type = IP::Type::NONE;
         hostname = "";
       };
 
@@ -38,7 +41,7 @@ namespace core { namespace io
     {
       for (int i = 0; i < IP::RESOLVER_MAX_QUERIES; i++)
       {
-        if (queue[i].status == IP::ResolverStatus::RESOLVER_STATUS_DONE)
+        if (queue[i].status == IP::ResolverStatus::DONE)
         {
           return i;
         }
@@ -46,8 +49,50 @@ namespace core { namespace io
       return IP::RESOLVER_INVALID_ID;
     }
 
-    std::mutex *mutex;
+    std::mutex mtx;
 
+    std::thread trd;
 
+    bool thread_abort;
+
+    void
+    resolve_queues()
+    {
+      for (auto i = 0; i < IP::RESOLVER_MAX_QUERIES; i++)
+      {
+        if (queue[i].status != IP::ResolverStatus::WAITING)
+        {
+          continue;
+        }
+
+        queue[i].response = core::base::Singleton<IP>::Instance().resolve_hostname(queue[i].hostname, queue[i].type);
+
+        if (!queue[i].response.is_valid())
+        {
+          queue[i].status = IP::ResolverStatus::ERROR;
+        }
+        else
+        {
+          queue[i].status = IP::ResolverStatus::DONE;
+        }
+      }
+    }
+
+    static void thread_function(std::shared_ptr<_IpResolverPrivate> resolver)
+    {
+      while (!resolver->thread_abort)
+      {
+        resolver->mtx->lock();
+        resolver->resolve_queues();
+        resolver->mtx.unlock();
+      }
+    }
+
+    std::map<std::string, IpAddress> cache;
+
+    static std::string get_cache_key(const std::string &hostname, IP::Type type)
+    {
+      return std::to_string(type) + hostname;
+    }
   };
 }} // namespace core / io
--- core/io/ip.h
+++ core/io/ip.h
@@ -7,25 +7,25 @@
 
 namespace core { namespace io
 {
-  struct _IP_ResolverPrivate;
+  struct _IpResolverPrivate;
 
   class IP
   {
   public:
     enum class ResolverStatus : int
     {
-      RESOLVER_STATUS_NONE = 0,
-      RESOLVER_STATUS_WAITING = 1,
-      RESOLVER_STATUS_DONE = 2,
-      RESOLVER_STATUS_ERROR = 3
+      NONE = 0,
+      WAITING = 1,
+      DONE = 2,
+      ERROR = 3
     };
 
     enum class Type : int
     {
-      TYPE_NONE = 0,
-      TYPE_IPV4 = 1,
-      TYPE_IPV6 = 2,
-      TYPE_ANY = 3
+      NONE = 0,
+      IPV4 = 1,
+      IPV6 = 2,
+      ANY = 3
     };
 
     constexpr int RESOLVER_MAX_QUERIES = 32;
@@ -34,42 +34,42 @@ namespace core { namespace io
 
     using ResolverID = int;
 
-  private:
-    _IP_ResolvePrivate *resolver;
-
-  protected:
-    static IP *singleton;
-
-    static void _bind_methods();
-
-    virtual IP_Address _resolve_hostname(const std::string &hostname, Type type = Type::TYPE_ANY) = 0;
-
-    std::vector _get_local_addresses() const;
-
-    static IP *(*_create)();
-
   public:
-    static IP *get_singleton();
-
-    static IP *create();
-
-    IP_Address resolve_hostname(const std::string &hostname, Type type = Type::TYPE_ANY);
+    IpAddress resolve_hostname(const std::string &hostname, Type type = Type::TYPE_ANY);
 
     ResolverID resolve_hostname_queue_item(const std::string &hostname, Type type = TYPE_ANY);
 
     ResolverStatus get_resolve_item_status(ResolverID id) const;
 
-    IP_Address get_resolve_item_address(ResolverID id) const;
+    IpAddress get_resolve_item_address(ResolverID id) const;
 
-    virtual void get_local_addresses(std::list<IP_Address> &addresses) const = 0;
+    virtual void get_local_addresses(std::list<IpAddress> &addresses) const = 0;
 
     void erase_resolve_item(ResolverID id);
 
     void clear_cache(const std::string &hostname = "");
 
+    // static IP *get_singleton();
+
+    // static IP *create();
+
     IP();
 
     ~IP();
+
+  protected:
+    // static IP *singleton;
+
+    // static void _bind_methods();
+
+    virtual IpAddress _resolve_hostname(const std::string &hostname, Type type = Type::TYPE_ANY) = 0;
+
+    std::vector _get_local_addresses() const;
+
+    // static IP *(*_create)();
+
+  private:
+    _IP_ResolvePrivate *resolver;
   };
 }} // namespace core / io
 
