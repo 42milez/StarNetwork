569a93714ef1e8c26b15e7b82c92969000f30e32
Akihiro TAKASE <42milez@gmail.com>
2019-02-07 23:02:21 +0900

    Add _parse_hex

--- core/io/ip_address.cpp
+++ core/io/ip_address.cpp
@@ -14,10 +14,9 @@ namespace core { namespace io
 
         if (is_ipv4())
         {
-
-            return std::to_string(static_cast<integer>(std::get<IP128_8>(_field)[12])) +
-                   std::to_string(static_cast<integer>(std::get<IP128_8>(_field)[13])) +
-                   std::to_string(static_cast<integer>(std::get<IP128_8>(_field)[14])) +
+            return std::to_string(static_cast<integer>(std::get<IP128_8>(_field)[12])) + "." +
+                   std::to_string(static_cast<integer>(std::get<IP128_8>(_field)[13])) + "." +
+                   std::to_string(static_cast<integer>(std::get<IP128_8>(_field)[14])) + "." +
                    std::to_string(static_cast<integer>(std::get<IP128_8>(_field)[15]));
         }
 
@@ -36,5 +35,50 @@ namespace core { namespace io
         }
 
         return ret.str();
+    }
+
+    static void
+    _parse_hex(const std::string &str, int start, uint8_t dst)
+    {
+        uint16_t ret = 0;
+
+        for (auto i = start; i < start + 4; i++)
+        {
+            if (i >= str.length())
+            {
+                break;
+            }
+
+            auto n = 0;
+            auto c = str[i];
+
+            if (c >= '0' && c <= '9')
+            {
+                n = c - '0';
+            }
+            else if (c >= 'a' && c <= 'f')
+            {
+                n = 10 + (c - 'a');
+            }
+            else if (c >= 'A' && c <= 'F')
+            {
+                n = 10 + (c - 'A');
+            }
+            else if (c == ':')
+            {
+                break;
+            }
+            else
+            {
+                // ToDo: logging
+                // ...
+            }
+
+            ret = ret << 4;
+            ret += n;
+        }
+
+        dst[0] = ret >> 8;
+        dst[1] = ret & 0xff;
     }
 }} // namespace core /io
