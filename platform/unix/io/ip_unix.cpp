d1757bbb054f6d67447c2a9c56d72d914eef73c9
Akihiro TAKASE <42milez@gmail.com>
2019-02-16 00:31:00 +0900

    Add _sockaddr2ip()

--- platform/unix/io/ip_unix.cpp
+++ platform/unix/io/ip_unix.cpp
@@ -1,1 +1,35 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+
 #include "ip_unix.h"
+
+#include "platform/unix/io/ip_unix.h"
+
+namespace platform { namespace unix { namespace io
+{
+    static core::io::IpAddress
+    _sockaddr2ip(struct sockaddr &addr)
+    {
+        core::io::IpAddress ip;
+
+        if (addr.sa_family == AF_INET)
+        {
+            auto &addr_in = reinterpret_cast<struct sockaddr_in &>(addr);
+
+            auto octet1 = static_cast<uint8_t>(addr_in.sin_addr.s_addr >> 24);
+            auto octet2 = static_cast<uint8_t>(addr_in.sin_addr.s_addr >> 16);
+            auto octet3 = static_cast<uint8_t>(addr_in.sin_addr.s_addr >> 8);
+            auto octet4 = static_cast<uint8_t>(addr_in.sin_addr.s_addr);
+
+            ip.set_ipv4({octet1, octet2, octet3, octet4});
+        }
+        else if (addr.sa_family == AF_INET6)
+        {
+            auto &addr_in6 = reinterpret_cast<struct sockaddr_in6 &>(addr);
+
+            ip.set_ipv6(addr_in6.sin6_addr.s6_addr);
+        }
+
+        return ip;
+    }
+}}} // namespace platform / unix / io
--- platform/unix/io/ip_unix.h
+++ platform/unix/io/ip_unix.h
@@ -2,12 +2,14 @@
 #define P2P_TECHDEMO_PLATFORM_UNIX_IO_IP_UNIX_H
 
 #include "core/io/ip.h"
+#include "core/io/ip_address.h"
 
 namespace platform { namespace unix { namespace io
 {
     class IpUnix : public core::io::IP
     {
-
+    private:
+        virtual core::io::IpAddress _resolve_hostname(const std::string &hostname, core::io::IP::Type type);
     };
 }}} // namespace platform / unix / io
 
