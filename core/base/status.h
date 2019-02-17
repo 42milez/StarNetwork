6851756fadcf3e39f78dee6b34b5efd03b58f0f2
Akihiro TAKASE <42milez@gmail.com>
2019-02-17 18:38:34 +0900

    Add enum class Status

---
+++ core/base/status.h
@@ -0,0 +1,12 @@
+#ifndef P2P_TECHDEMO_CORE_STATUS_H
+#define P2P_TECHDEMO_CORE_STATUS_H
+
+namespace core { namespace base
+{
+    enum class Status : int {
+        OK,
+        ERROR,
+    };
+}} // namespace core / base
+
+#endif // P2P_TECHDEMO_CORE_STATUS_H
