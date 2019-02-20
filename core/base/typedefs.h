a9b065d60d9b97ed548311c9fe62401cbc884f3e
Akihiro TAKASE <42milez@gmail.com>
2019-02-21 00:33:45 +0900

    Add typedefs.h

---
+++ core/base/typedefs.h
@@ -0,0 +1,7 @@
+#ifndef P2P_TECHDEMO_CORE_BASE_TYPEDEFS_H
+#define P2P_TECHDEMO_CORE_BASE_TYPEDEFS_H
+
+#define likely(x) __builtin_expect(!!(x), 1)
+#define unlikely(x) __builtin_expect(!!(x), 0)
+
+#endif // P2P_TECHDEMO_CORE_BASE_TYPEDEFS_H
