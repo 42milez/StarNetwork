e7883c1ecf90f411a458d0eb04a1003270363631
Akihiro TAKASE <42milez@gmail.com>
2019-02-28 20:34:00 +0900

    Add SocketUtil:: is_no_messages_to_read()

--- core/io/socket_util.cpp
+++ core/io/socket_util.cpp
@@ -8,6 +8,12 @@ SocketUtil::is_connection_reset_on_recv(ssize_t read_byte_count)
     return read_byte_count == 0 || (read_byte_count == -1 && last_error() == ECONNRESET);
 }
 
+bool
+SocketUtil::is_no_messages_to_read(ssize_t read_byte_count)
+{
+    return read_byte_count == -1 && SocketUtil::last_error() == EAGAIN;
+}
+
 int
 SocketUtil::last_error()
 {
