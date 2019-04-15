f24f14b9c2428c172d647eebc8939b72a1317a27
Akihiro TAKASE <42milez@gmail.com>
2019-04-15 22:57:33 +0900

    Add hash32()

---
+++ core/hash.cpp
@@ -0,0 +1,17 @@
+#include <string>
+
+#include <uuid/uuid.h>
+
+#include "hash.h"
+
+size_t
+hash32()
+{
+    uuid_t in;
+    uuid_string_t out;
+
+    uuid_generate_time(in);
+    uuid_unparse(in, out);
+
+    return std::hash<std::string>{}(std::string(out));
+}
