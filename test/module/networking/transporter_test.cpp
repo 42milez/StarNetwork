d23ea8575dbc1a215bcf3090c2d8ac0919e82ef6
Akihiro TAKASE <42milez@gmail.com>
2019-04-22 23:15:59 +0900

    Add a test

--- test/module/networking/transporter_test.cpp
+++ test/module/networking/transporter_test.cpp
@@ -9,6 +9,13 @@
 
 TEST_CASE("", "")
 {
-    std::unique_ptr<Transporter> transporter = std::make_unique<Transporter>();
-    transporter->create_server(8888, 32, 100, 100);
+    std::unique_ptr<Transporter> server = std::make_unique<Transporter>();
+    auto ret_server = server->create_server(8888, 32, 100, 100);
+
+    REQUIRE(ret_server == Error::OK);
+
+    std::unique_ptr<Transporter> client = std::make_unique<Transporter>();
+    auto ret_client = client->create_client("::127.0.0.1", 8888, 100, 100, 8889);
+
+    REQUIRE(ret_client == Error::OK);
 }
