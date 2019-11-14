cc6754fa9e668868ce819f7178b3ed4abddf37e8
Akihiro TAKASE <42milez@gmail.com>
2019-11-14 22:29:42 +0900

    clean up

--- test/e2e/rudp/send_reliable_command/main.cpp
+++ test/e2e/rudp/send_reliable_command/main.cpp
@@ -22,38 +22,11 @@ namespace
     { usleep(SLEEP_DURATION); }
 }
 
-class HostFixture
-{
-public:
-    HostFixture()
-    {
-        RUdpAddress address;
-        address.port(8888);
-        host_ = std::make_unique<RUdpHost>(address, SysCh::MAX, 32, 100, 100);
-        core::Singleton<core::Logger>::Instance().Init("Send Reliable Command Test");
-    }
-
-    void
-    Broadcast(SysCh ch, std::shared_ptr<RUdpSegment> &segment)
-    { host_->Broadcast(ch, segment); }
-
-    RUdpPeerState
-    PeerState(size_t idx)
-    { return host_->PeerState(0); }
-
-    EventStatus
-    Service(std::unique_ptr<RUdpEvent> &event, uint32_t timeout)
-    { return host_->Service(event, timeout); }
-
-private:
-    std::shared_ptr<RUdpHost> host_;
-};
-
 // テスト内容
-// 1. Guest1をHostに接続
-// 2. Guest2をHostに接続
-// 3. Guest1からHostにデータ送信（Host側で正常に受信できたことを検証）
-// 4. Guest2からHostにデータ送信（Host側で正常に受信できたことを検証）
+// 1. Guest1からHostに接続
+// 2. Guest2からHostに接続
+// 3. Guest1からHostにデータ送信（Host側で正常に受信できることを検証）
+// 4. Guest2からHostにデータ送信（Host側で正常に受信できることを検証）
 TEST_CASE("guest sends reliable command to host", "[reliable command]")
 {
     core::Singleton<core::Logger>::Instance().Init("Send Reliable Command Test");
