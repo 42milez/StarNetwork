ff67f4114479db180ab74b28de0b630fa3c07b92
Akihiro TAKASE <42milez@gmail.com>
2019-12-15 22:17:00 +0900

    update

--- lib/rudp/protocol/RUdpProtocolType.h
+++ lib/rudp/protocol/RUdpProtocolType.h
@@ -6,130 +6,130 @@
 
 using RUdpProtocolHeader = struct RUdpProtocolHeader
 {
-    uint16_t peer_id;
-    uint16_t sent_time;
+  uint16_t peer_id;
+  uint16_t sent_time;
 };
 
 using RUdpProtocolCommandHeader = struct RUdpProtocolCommandHeader
 {
-    uint8_t command;
-    uint8_t channel_id;
-    uint16_t reliable_sequence_number;
+  uint8_t command;
+  uint8_t channel_id;
+  uint16_t reliable_sequence_number;
 };
 
 using RUdpProtocolAcknowledge = struct RUdpProtocolAcknowledge
 {
-    RUdpProtocolCommandHeader header;
-    uint16_t received_reliable_sequence_number;
-    uint16_t received_sent_time;
+  RUdpProtocolCommandHeader header;
+  uint16_t received_reliable_sequence_number;
+  uint16_t received_sent_time;
 };
 
 using RUdpProtocolConnect = struct RUdpProtocolConnect
 {
-    RUdpProtocolCommandHeader header;
-    uint32_t outgoing_peer_id;
-    uint8_t incoming_session_id;
-    uint8_t outgoing_session_id;
-    uint32_t mtu;
-    uint32_t window_size;
-    uint32_t channel_count;
-    uint32_t incoming_bandwidth;
-    uint32_t outgoing_bandwidth;
-    uint32_t segment_throttle_interval;
-    uint32_t segment_throttle_acceleration;
-    uint32_t segment_throttle_deceleration;
-    uint32_t connect_id;
-    uint32_t data;
+  RUdpProtocolCommandHeader header;
+  uint16_t outgoing_peer_id;
+  uint8_t incoming_session_id;
+  uint8_t outgoing_session_id;
+  uint32_t mtu;
+  uint32_t window_size;
+  uint32_t channel_count;
+  uint32_t incoming_bandwidth;
+  uint32_t outgoing_bandwidth;
+  uint32_t segment_throttle_interval;
+  uint32_t segment_throttle_acceleration;
+  uint32_t segment_throttle_deceleration;
+  uint32_t connect_id;
+  uint32_t data;
 };
 
 using RUdpProtocolVerifyConnect = struct RUdpProtocolVerifyConnect
 {
-    RUdpProtocolCommandHeader header;
-    uint16_t outgoing_peer_id;
-    uint8_t incoming_session_id;
-    uint8_t outgoing_session_id;
-    uint32_t mtu;
-    uint32_t window_size;
-    uint32_t channel_count;
-    uint32_t incoming_bandwidth;
-    uint32_t outgoing_bandwidth;
-    uint32_t segment_throttle_interval;
-    uint32_t segment_throttle_acceleration;
-    uint32_t segment_throttle_deceleration;
-    uint32_t connect_id;
+  RUdpProtocolCommandHeader header;
+  uint16_t outgoing_peer_id;
+  uint8_t incoming_session_id;
+  uint8_t outgoing_session_id;
+  uint32_t mtu;
+  uint32_t window_size;
+  uint32_t channel_count;
+  uint32_t incoming_bandwidth;
+  uint32_t outgoing_bandwidth;
+  uint32_t segment_throttle_interval;
+  uint32_t segment_throttle_acceleration;
+  uint32_t segment_throttle_deceleration;
+  uint32_t connect_id;
 };
 
 using RUdpProtocolDisconnect = struct RUdpProtocolDisconnect
 {
-    RUdpProtocolCommandHeader header;
-    uint32_t data;
+  RUdpProtocolCommandHeader header;
+  uint32_t data;
 };
 
 using RUdpProtocolPing = struct RUdpProtocolPing
 {
-    RUdpProtocolCommandHeader header;
+  RUdpProtocolCommandHeader header;
 };
 
 using RUdpProtocolSendReliable = struct RUdpProtocolSendReliable
 {
-    RUdpProtocolCommandHeader header;
-    uint16_t data_length;
+  RUdpProtocolCommandHeader header;
+  uint16_t data_length;
 };
 
 using RUdpProtocolSendUnreliable = struct RUdpProtocolSendUnreliable
 {
-    RUdpProtocolCommandHeader header;
-    uint16_t unreliable_sequence_number;
-    uint16_t data_length;
+  RUdpProtocolCommandHeader header;
+  uint16_t unreliable_sequence_number;
+  uint16_t data_length;
 };
 
 using RUdpProtocolSendUnsequenced = struct RUdpProtocolSendUnsequenced
 {
-    RUdpProtocolCommandHeader header;
-    uint16_t unsequenced_group;
-    uint16_t data_length;
+  RUdpProtocolCommandHeader header;
+  uint16_t unsequenced_group;
+  uint16_t data_length;
 };
 
 using RUdpProtocolSendFragment = struct RUdpProtocolSendFragment
 {
-    RUdpProtocolCommandHeader header;
-    uint16_t start_sequence_number;
-    uint16_t data_length;
-    uint32_t fragment_count;
-    uint32_t fragment_number;
-    uint32_t total_length;
-    uint32_t fragment_offset;
+  RUdpProtocolCommandHeader header;
+  uint16_t start_sequence_number;
+  uint16_t data_length;
+  uint32_t fragment_count;
+  uint32_t fragment_number;
+  uint32_t total_length;
+  uint32_t fragment_offset;
 };
 
 using RUdpProtocolBandwidthLimit = struct RUdpProtocolBandwidthLimit
 {
-    RUdpProtocolCommandHeader header;
-    uint32_t incoming_bandwidth;
-    uint32_t outgoing_bandwidth;
+  RUdpProtocolCommandHeader header;
+  uint32_t incoming_bandwidth;
+  uint32_t outgoing_bandwidth;
 };
 
 using RUdpProtocolThrottleConfigure = struct RUdpProtocolThrottleConfigure
 {
-    RUdpProtocolCommandHeader header;
-    uint32_t segment_throttle_interval;
-    uint32_t segment_throttle_acceleration;
-    uint32_t segment_throttle_deceleration;
+  RUdpProtocolCommandHeader header;
+  uint32_t segment_throttle_interval;
+  uint32_t segment_throttle_acceleration;
+  uint32_t segment_throttle_deceleration;
 };
 
 using RUdpProtocolType = union RUdpProtocolType
 {
-    RUdpProtocolCommandHeader header;
-    RUdpProtocolAcknowledge acknowledge;
-    RUdpProtocolConnect connect;
-    RUdpProtocolVerifyConnect verify_connect;
-    RUdpProtocolDisconnect disconnect;
-    RUdpProtocolPing ping;
-    RUdpProtocolSendReliable send_reliable;
-    RUdpProtocolSendUnreliable send_unreliable;
-    RUdpProtocolSendUnsequenced send_unsequenced;
-    RUdpProtocolSendFragment send_fragment;
-    RUdpProtocolBandwidthLimit bandwidth_limit;
-    RUdpProtocolThrottleConfigure throttle_configure;
+  RUdpProtocolCommandHeader header;
+  RUdpProtocolAcknowledge acknowledge;
+  RUdpProtocolConnect connect;
+  RUdpProtocolVerifyConnect verify_connect;
+  RUdpProtocolDisconnect disconnect;
+  RUdpProtocolPing ping;
+  RUdpProtocolSendReliable send_reliable;
+  RUdpProtocolSendUnreliable send_unreliable;
+  RUdpProtocolSendUnsequenced send_unsequenced;
+  RUdpProtocolSendFragment send_fragment;
+  RUdpProtocolBandwidthLimit bandwidth_limit;
+  RUdpProtocolThrottleConfigure throttle_configure;
 };
 
 using RUdpProtocolTypeSP = std::shared_ptr<RUdpProtocolType>;
