dbf522eaea569c56bd5b8efdcf82ee7ea80d4d94
Akihiro TAKASE <42milez@gmail.com>
2020-01-10 01:21:13 +0900

    update

--- test/run.sh
+++ test/run.sh
@@ -7,4 +7,5 @@ cd /var/app/build || exit
 
 /var/app/build/test/e2e/rudp/basic_connection/basic_connection_test -r console -d yes --order lex
 /var/app/build/test/e2e/rudp/broadcast/broadcast_test -r console -d yes --order lex
+/var/app/build/test/e2e/rudp/packet_loss/packet_loss_test -r console -d yes --order lex
 /var/app/build/test/e2e/rudp/send_reliable_command/send_reliable_command_test -r console -d yes --order lex
