4e4717da974ae0bc325cce228ee29a345f176ed2
Akihiro TAKASE <42milez@gmail.com>
2020-01-13 01:46:13 +0900

    update

--- test/run_e2e_test.sh
+++ test/run_e2e_test.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-OUTPUT_PATH="${HOME}/test-results"
-
 cd /var/app/build || exit
 
 /usr/bin/cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -G "CodeBlocks - Unix Makefiles" /var/app
 /usr/bin/cmake --build /var/app/build --target all -- -j 4
 
-/var/app/build/test/e2e/rudp/basic_connection/basic_connection_test           -r xml -d yes --order lex -o "${OUTPUT_PATH}/e2e/rudp/basic_connection/basic_connection_test.xml"
-/var/app/build/test/e2e/rudp/broadcast/broadcast_test                         -r xml -d yes --order lex -o "${OUTPUT_PATH}/e2e/rudp/broadcast/broadcast_test.xml"
-/var/app/build/test/e2e/rudp/send_reliable_command/send_reliable_command_test -r xml -d yes --order lex -o "${OUTPUT_PATH}/e2e/rudp/send_reliable_command/send_reliable_command_test.xml"
+/var/app/build/test/e2e/rudp/basic_connection/basic_connection_test           -r console -d yes --order lex
+/var/app/build/test/e2e/rudp/broadcast/broadcast_test                         -r console -d yes --order lex
+/var/app/build/test/e2e/rudp/send_reliable_command/send_reliable_command_test -r console -d yes --order lex
