932c70aa78fe2f1d2fe4c33f42d1cc332a5e60b0
Akihiro TAKASE <42milez@gmail.com>
2020-01-12 20:22:33 +0900

    update

--- test/run_unit_test.sh
+++ test/run_unit_test.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+OUTPUT_PATH="${HOME}/catch-result"
+
 cd /var/app/build || exit
 
 /usr/bin/cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -G "CodeBlocks - Unix Makefiles" /var/app
 /usr/bin/cmake --build /var/app/build --target all -- -j 4
 
-/var/app/build/test/unit/lib/rudp/command/rudp_command_test -r console -d yes --order lex
+/var/app/build/test/unit/lib/rudp/command/rudp_command_test -d yes --order lex -r xml -o "${OUTPUT_PATH}/unit/lib/rudp/command/rudp_command_test.xml"
