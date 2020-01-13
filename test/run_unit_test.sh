75fa0e8906cd6b5ae7076c8233617ebe9be7f0f5
Akihiro TAKASE <42milez@gmail.com>
2020-01-13 15:29:48 +0900

    update

--- test/run_unit_test.sh
+++ test/run_unit_test.sh
@@ -10,4 +10,6 @@ if [[ -z ${CATCH_REPORTER} ]]; then
   REPORTER=${CATCH_REPORTER}
 fi
 
+echo "${REPORTER}"
+
 "${CMAKE_BUILD_DIR}"/test/unit/lib/rudp/command/rudp_command_test -r "${REPORTER}" -d yes --order lex -o "${TEST_RESULT_DIR}/unit_rudp_command_test.xml"
