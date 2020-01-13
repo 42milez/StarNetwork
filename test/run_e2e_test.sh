6cd10d00a1c585f2a3397a3a635ee35bf00e5398
Akihiro TAKASE <42milez@gmail.com>
2020-01-13 15:56:38 +0900

    update

--- test/run_e2e_test.sh
+++ test/run_e2e_test.sh
@@ -10,8 +10,6 @@ if [[ -n ${CATCH_REPORTER} ]]; then
   REPORTER=${CATCH_REPORTER}
 fi
 
-echo "${REPORTER}"
-
 "${CMAKE_BUILD_DIR}"/test/e2e/rudp/basic_connection/basic_connection_test           -r "${REPORTER}" -d yes --order lex -o "${TEST_RESULT_DIR}/e2e_basic_connection_test.xml"
 "${CMAKE_BUILD_DIR}"/test/e2e/rudp/broadcast/babroadcast_test                       -r "${REPORTER}" -d yes --order lex -o "${TEST_RESULT_DIR}/e2e_babroadcast_test.xml"
 "${CMAKE_BUILD_DIR}"/test/e2e/rudp/send_reliable_command/send_reliable_command_test -r "${REPORTER}" -d yes --order lex -o "${TEST_RESULT_DIR}/e2e_send_reliable_command_test.xml"
