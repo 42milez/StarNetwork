cc4483453acc09f26596eb5f965152fc0090e07e
Akihiro TAKASE <42milez@gmail.com>
2020-02-18 19:48:22 +0900

    tiny fix

--- docker/script/build.sh
+++ docker/script/build.sh
@@ -22,5 +22,5 @@ fi
 
 cd "${BUILD_DIR}" || exit
 
-/usr/bin/cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -G "CodeBlocks - Unix Makefiles" "${WORK_DIR}"
+/usr/bin/cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=/usr/bin/clang-9 -DCMAKE_CXX_COMPILER=/usr/bin/clang++-9 -G "CodeBlocks - Unix Makefiles" "${WORK_DIR}"
 /usr/bin/cmake --build "${BUILD_DIR}" --target all -- -j 4
