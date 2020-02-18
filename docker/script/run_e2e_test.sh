533421f82c4f88dd17992322b4090ad73487139a
Akihiro TAKASE <42milez@gmail.com>
2020-02-18 19:52:53 +0900

    clean up

--- docker/script/run_e2e_test.sh
+++ docker/script/run_e2e_test.sh
@@ -16,6 +16,6 @@ if [[ ${REPORTER} = "junit" ]]; then
   "${CMAKE_BUILD_DIR}"/bin/send_reliable_command_test -r junit -d yes --order lex -o "${TEST_RESULT_DIR}/e2e_send_reliable_command_test.xml"
 else
   "${CMAKE_BUILD_DIR}"/bin/basic_connection_test      -r console -d yes --order lex
-  "${CMAKE_BUILD_DIR}"/bin/broadcast_test           -r console -d yes --order lex
+  "${CMAKE_BUILD_DIR}"/bin/broadcast_test             -r console -d yes --order lex
   "${CMAKE_BUILD_DIR}"/bin/send_reliable_command_test -r console -d yes --order lex
 fi
