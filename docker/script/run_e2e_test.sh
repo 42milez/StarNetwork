c2bec68bbaee973cf004d3eb048d70fe10bdd517
Akihiro TAKASE <42milez@gmail.com>
2020-02-18 19:33:44 +0900

    tiny fix

--- docker/script/run_e2e_test.sh
+++ docker/script/run_e2e_test.sh
@@ -12,10 +12,10 @@ fi
 
 if [[ ${REPORTER} = "junit" ]]; then
   "${CMAKE_BUILD_DIR}"/bin/basic_connection_test      -r junit -d yes --order lex -o "${TEST_RESULT_DIR}/e2e_basic_connection_test.xml"
-  "${CMAKE_BUILD_DIR}"/bin/babroadcast_test           -r junit -d yes --order lex -o "${TEST_RESULT_DIR}/e2e_babroadcast_test.xml"
+  "${CMAKE_BUILD_DIR}"/bin/broadcast_test             -r junit -d yes --order lex -o "${TEST_RESULT_DIR}/e2e_broadcast_test.xml"
   "${CMAKE_BUILD_DIR}"/bin/send_reliable_command_test -r junit -d yes --order lex -o "${TEST_RESULT_DIR}/e2e_send_reliable_command_test.xml"
 else
   "${CMAKE_BUILD_DIR}"/bin/basic_connection_test      -r console -d yes --order lex
-  "${CMAKE_BUILD_DIR}"/bin/babroadcast_test           -r console -d yes --order lex
+  "${CMAKE_BUILD_DIR}"/bin/broadcast_test           -r console -d yes --order lex
   "${CMAKE_BUILD_DIR}"/bin/send_reliable_command_test -r console -d yes --order lex
 fi
