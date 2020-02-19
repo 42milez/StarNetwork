9c77d92dc7cf1cb819c37de1b577bf46105dc9ad
Akihiro TAKASE <42milez@gmail.com>
2020-02-19 20:39:30 +0900

    Clean up

--- docker/script/run_e2e_test.sh
+++ docker/script/run_e2e_test.sh
@@ -11,9 +11,9 @@ if [[ -n ${CATCH_REPORTER} ]]; then
 fi
 
 if [[ ${REPORTER} = "junit" ]]; then
-  "${CMAKE_BUILD_DIR}"/bin/basic_connection_test -r junit -d yes --order lex -o "${TEST_RESULT_DIR}/e2e_basic_connection_test.xml"
-  "${CMAKE_BUILD_DIR}"/bin/broadcast_test        -r junit -d yes --order lex -o "${TEST_RESULT_DIR}/e2e_broadcast_test.xml"
-  "${CMAKE_BUILD_DIR}"/bin/reliable_command_test -r junit -d yes --order lex -o "${TEST_RESULT_DIR}/e2e_reliable_command_test.xml"
+  "${CMAKE_BUILD_DIR}"/bin/basic_connection_test -r junit -d yes --order lex -o "${TEST_RESULT_DIR}/feature_basic_connection_test.xml"
+  "${CMAKE_BUILD_DIR}"/bin/broadcast_test        -r junit -d yes --order lex -o "${TEST_RESULT_DIR}/feature_broadcast_test.xml"
+  "${CMAKE_BUILD_DIR}"/bin/reliable_command_test -r junit -d yes --order lex -o "${TEST_RESULT_DIR}/feature_reliable_command_test.xml"
 else
   "${CMAKE_BUILD_DIR}"/bin/basic_connection_test -r console -d yes --order lex
   "${CMAKE_BUILD_DIR}"/bin/broadcast_test        -r console -d yes --order lex
