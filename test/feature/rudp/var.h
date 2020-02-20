aecccf0adfd9b2d69676512b48f0deb03a5abccb
Akihiro TAKASE <42milez@gmail.com>
2020-02-20 22:17:22 +0900

    set AlignConsecutiveAssignments to true

--- test/feature/rudp/var.h
+++ test/feature/rudp/var.h
@@ -1,20 +1,20 @@
 namespace connection
 {
-    const uint16_t HOST_PORT = 10000;
+    const uint16_t HOST_PORT   = 10000;
     const uint16_t GUEST1_PORT = 10001;
     const uint16_t GUEST2_PORT = 10002;
 } // namespace connection
 
 namespace broadcast
 {
-    const uint16_t HOST_PORT = 10010;
+    const uint16_t HOST_PORT   = 10010;
     const uint16_t GUEST1_PORT = 10011;
     const uint16_t GUEST2_PORT = 10012;
 } // namespace broadcast
 
 namespace reliable_command
 {
-    const uint16_t HOST_PORT = 10020;
+    const uint16_t HOST_PORT   = 10020;
     const uint16_t GUEST1_PORT = 10021;
     const uint16_t GUEST2_PORT = 10022;
 } // namespace reliable_command
