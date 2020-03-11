9b72730f87f35b61aa32630d51f65af8c51dc7f8
Akihiro TAKASE <42milez@gmail.com>
2020-03-11 20:59:38 +0900

    separate sanitize steps

--- docker/script/test/report_all_coverage.sh
+++ docker/script/test/report_all_coverage.sh
@@ -5,8 +5,6 @@ DOCKER_SCRIPT_DIR=$(dirname $(dirname "${BASH_SOURCE}"))
 . "${DOCKER_SCRIPT_DIR}/config.bash"
 . "${DOCKER_SCRIPT_DIR}/var.bash"
 
-"${BUILD_DIR}"/bin/all_tests >/dev/null 2>&1
-
 llvm-profdata-9 merge -sparse "${WORK_DIR}/default.profraw" -o "${WORK_DIR}/default.profdata"
 llvm-cov-9 export "${BUILD_DIR}/bin/all_tests" -instr-profile="${WORK_DIR}/default.profdata" > "${COVERAGE_ALL_PATH}"
 
