b65c426af885ffa9fcf6c7babb03c3b032568cb1
Akihiro TAKASE <42milez@gmail.com>
2020-03-11 21:09:45 +0900

    tiny change

--- docker/script/test/report_all_coverage.sh
+++ docker/script/test/report_all_coverage.sh
@@ -3,10 +3,16 @@
 DOCKER_SCRIPT_DIR=$(dirname $(dirname "${BASH_SOURCE}"))
 
 . "${DOCKER_SCRIPT_DIR}/config.bash"
+. "${DOCKER_SCRIPT_DIR}/handler.bash"
 . "${DOCKER_SCRIPT_DIR}/var.bash"
 
 llvm-profdata-9 merge -sparse "${WORK_DIR}/default.profraw" -o "${WORK_DIR}/default.profdata"
+
+EXIT_ON_FAIL $? 'llvm-profdata failed'
+
 llvm-cov-9 export "${BUILD_DIR}/bin/all_tests" -instr-profile="${WORK_DIR}/default.profdata" > "${COVERAGE_ALL_PATH}"
+
+EXIT_ON_FAIL $? 'llvm-cov failed'
 
 rm "${WORK_DIR}/default.profraw"
 rm "${WORK_DIR}/default.profdata"
