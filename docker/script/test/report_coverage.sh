7983a97f958a6ebb2b70176f7ed41e21b20b2741
Akihiro TAKASE <42milez@gmail.com>
2020-03-12 01:23:51 +0900

    use grcov to report code coverage

---
+++ docker/script/test/report_coverage.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+DOCKER_SCRIPT_DIR=$(dirname $(dirname "${BASH_SOURCE}"))
+
+. "${DOCKER_SCRIPT_DIR}/config.bash"
+. "${DOCKER_SCRIPT_DIR}/handler.bash"
+. "${DOCKER_SCRIPT_DIR}/var.bash"
+
+wget "${GRCOV_LOCATION}/${GRCOV_VERSION}/${GRCOV_ASSET}"
+
+EXIT_ON_FAIL $? 'Download grcov Failed'
+
+tar xf "${GRCOV_ASSET}"
+
+grcov --llvm . --token unused --commit-sha unused > "${COVERAGE_FILE_PATH}"
--- docker/script/var.bash
+++ docker/script/var.bash
@@ -8,3 +8,7 @@ export CMAKE_CXX_COMPILER=/usr/bin/clang++-9
 export CMAKE_CXX_FLAGS=''
 export CMAKE_CXX_FLAGS_DEBUG='-DDEBUG -g -O0'
 export CMAKE_GENERATOR='CodeBlocks - Unix Makefiles'
+
+export GRCOV_LOCATION='https://github.com/mozilla/grcov/releases/download'
+export GRCOV_VERSION='v0.5.9'
+export GRCOV_ASSET='grcov-linux-x86_64.tar.bz2'
