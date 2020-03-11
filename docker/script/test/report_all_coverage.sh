fdf334ad5f73589b06989509487ee299ba1614d3
Akihiro TAKASE <42milez@gmail.com>
2020-03-11 20:13:33 +0900

    tiny fix

--- docker/script/test/report_all_coverage.sh
+++ docker/script/test/report_all_coverage.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
+DOCKER_SCRIPT_DIR=$(dirname $(dirname "${BASH_SOURCE}"))
+
+. "${DOCKER_SCRIPT_DIR}/config.bash"
+. "${DOCKER_SCRIPT_DIR}/var.bash"
+
 "${BUILD_DIR}"/bin/all_tests >/dev/null 2>&1
 
-llvm-profdata-9 merge -sparse "${BUILD_DIR}/bin/default.profraw" -o "${BUILD_DIR}/default.profdata"
+llvm-profdata-9 merge -sparse "${WORK_DIR}/default.profraw" -o "${WORK_DIR}/default.profdata"
+llvm-cov-9 export "${BUILD_DIR}/bin/all_tests" -instr-profile="${WORK_DIR}/default.profdata" > "${COVERAGE_ALL_PATH}"
 
-llvm-cov-9 export "${BUILD_DIR}/bin/all_tests" -instr-profile="${BUILD_DIR}/default.profdata" > "${COVERAGE_ALL_PATH}"
+rm "${WORK_DIR}/default.profraw"
+rm "${WORK_DIR}/default.profdata"
