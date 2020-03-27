fc0b917e7aa9dc63f94883a4b4e0bdc03d7f0366
Akihiro TAKASE <42milez@gmail.com>
2020-03-27 22:58:04 +0900

    change test port

--- test/feature/rudp/var.h
+++ test/feature/rudp/var.h
@@ -3,9 +3,9 @@
 
 namespace test
 {
-    const uint16_t HOST_PORT   = 10000;
-    const uint16_t GUEST1_PORT = 10001;
-    const uint16_t GUEST2_PORT = 10002;
+    const uint16_t HOST_PORT   = 49152;
+    const uint16_t GUEST1_PORT = 49153;
+    const uint16_t GUEST2_PORT = 49154;
 
     const uint32_t DEFAULT_TIMEOUT = 1000; // msec
 } // namespace test
