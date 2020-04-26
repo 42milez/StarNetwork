57c5f83d468d5cee56d68655c0eec409a0520706
Akihiro TAKASE <42milez@gmail.com>
2020-04-26 19:17:28 +0900

    support release build

--- docker/script/var.bash
+++ docker/script/var.bash
@@ -2,11 +2,11 @@
 
 export WORK_DIR=/var/app
 
-export CMAKE_BUILD_TYPE=Debug
 export CMAKE_C_COMPILER=/usr/bin/clang-9
 export CMAKE_CXX_COMPILER=/usr/bin/clang++-9
 export CMAKE_CXX_FLAGS=''
 export CMAKE_CXX_FLAGS_DEBUG='-DDEBUG -g -O0'
+export CMAKE_CXX_FLAGS_RELEASE='-g -O1'
 export CMAKE_GENERATOR='CodeBlocks - Unix Makefiles'
 
 export GRCOV_LOCATION='https://github.com/mozilla/grcov/releases/download'
