11fa168f77708eb7b5961c800163446856d0a8e5
Akihiro TAKASE <42milez@gmail.com>
2020-04-28 01:59:09 +0900

    clean up

--- demo.bash
+++ demo.bash
@@ -36,38 +36,43 @@ tmux_run_command()
 
 : 'SPLIT WINDOW INTO 3 PAINS' &&
 {
-    tmux split-window -v -t "${TMUX_SESSION_NAME}" ; short_sleep
-    tmux_move_pain "${CLIENT1_PAIN}"               ; short_sleep
-    tmux split-window -h                           ; short_sleep
+  tmux split-window -v -t "${TMUX_SESSION_NAME}" ; short_sleep
+  tmux_move_pain "${CLIENT1_PAIN}"               ; short_sleep
+  tmux split-window -h                           ; short_sleep
 }
 
-: 'RUN DOCKER CONTAINERS' &&
+DOCKER_COMPOSE_FILE='docker-compose.release.yml'
+DOCKER_SERVICE_NAME='demo'
+
+: 'RUN CONTAINERS' &&
 {
-    tmux_move_pain "${CLIENT1_PAIN}"                                          ; short_sleep
-    tmux_run_command "cd ${LOCAL_WORK_DIR}"                                   ; short_sleep
-    tmux_run_command 'dc -f docker-compose.dev.yml exec dev_server /bin/bash' ; long_sleep
-    tmux_run_command "cd ${REMOTE_WORK_DIR}"                                  ; short_sleep
-    tmux_run_command 'clear'                                                  ; short_sleep
+  docker-compose -f docker-compose.release.yml up -d demo
 
-    tmux_move_pain "${CLIENT2_PAIN}"                                          ; short_sleep
-    tmux_run_command "cd ${LOCAL_WORK_DIR}"                                   ; short_sleep
-    tmux_run_command 'dc -f docker-compose.dev.yml exec dev_server /bin/bash' ; long_sleep
-    tmux_run_command "cd ${REMOTE_WORK_DIR}"                                  ; short_sleep
-    tmux_run_command 'clear'                                                  ; short_sleep
+  tmux_move_pain "${CLIENT1_PAIN}"                                                                  ; short_sleep
+  tmux_run_command "cd ${LOCAL_WORK_DIR}"                                                           ; short_sleep
+  tmux_run_command "docker-compose -f ${DOCKER_COMPOSE_FILE} exec ${DOCKER_SERVICE_NAME} /bin/bash" ; long_sleep
+  tmux_run_command "cd ${REMOTE_WORK_DIR}"                                                          ; short_sleep
+  tmux_run_command 'clear'                                                                          ; short_sleep
 
-    tmux_move_pain "${SERVER_PAIN}"                                           ; short_sleep
-    tmux_run_command "cd ${LOCAL_WORK_DIR}"                                   ; short_sleep
-    tmux_run_command 'dc -f docker-compose.dev.yml exec dev_server /bin/bash' ; long_sleep
-    tmux_run_command "cd ${REMOTE_WORK_DIR}"                                  ; short_sleep
-    tmux_run_command 'clear'                                                  ; short_sleep
+  tmux_move_pain "${CLIENT2_PAIN}"                                                                  ; short_sleep
+  tmux_run_command "cd ${LOCAL_WORK_DIR}"                                                           ; short_sleep
+  tmux_run_command "docker-compose -f ${DOCKER_COMPOSE_FILE} exec ${DOCKER_SERVICE_NAME} /bin/bash" ; long_sleep
+  tmux_run_command "cd ${REMOTE_WORK_DIR}"                                                          ; short_sleep
+  tmux_run_command 'clear'                                                                          ; short_sleep
+
+  tmux_move_pain "${SERVER_PAIN}"                                                                   ; short_sleep
+  tmux_run_command "cd ${LOCAL_WORK_DIR}"                                                           ; short_sleep
+  tmux_run_command "docker-compose -f ${DOCKER_COMPOSE_FILE} exec ${DOCKER_SERVICE_NAME} /bin/bash" ; long_sleep
+  tmux_run_command "cd ${REMOTE_WORK_DIR}"                                                          ; short_sleep
+  tmux_run_command 'clear'                                                                          ; short_sleep
 }
 
 BINARY_NAME='p2p_techdemo'
 
 : 'RUN SERVER' &&
 {
-    tmux_move_pain "${SERVER_PAIN}"                   ; short_sleep
-    tmux_run_command "./${BINARY_NAME} --mode server" ; long_sleep
+  tmux_move_pain "${SERVER_PAIN}"                   ; short_sleep
+  tmux_run_command "./${BINARY_NAME} --mode server" ; long_sleep
 }
 
 CLIENT1_PORT=49153
@@ -75,33 +80,33 @@ CLIENT2_PORT=49154
 
 : 'RUN CLIENTS' &&
 {
-    tmux_move_pain "${CLIENT1_PAIN}"                                         ; short_sleep
-    tmux_run_command "./${BINARY_NAME} --mode client --port ${CLIENT1_PORT}" ; long_sleep
+  tmux_move_pain "${CLIENT1_PAIN}"                                         ; short_sleep
+  tmux_run_command "./${BINARY_NAME} --mode client --port ${CLIENT1_PORT}" ; long_sleep
 
-    tmux_move_pain "${CLIENT2_PAIN}"                                         ; short_sleep
-    tmux_run_command "./${BINARY_NAME} --mode client --port ${CLIENT2_PORT}" ; long_sleep
+  tmux_move_pain "${CLIENT2_PAIN}"                                         ; short_sleep
+  tmux_run_command "./${BINARY_NAME} --mode client --port ${CLIENT2_PORT}" ; long_sleep
 }
 
 : 'SEND A MESSAGE FROM CLIENT1 (1/2)' &&
 {
-    tmux_move_pain "${CLIENT1_PAIN}" ; short_sleep
-    tmux_run_command "hello1"        ; short_sleep
+  tmux_move_pain "${CLIENT1_PAIN}" ; short_sleep
+  tmux_run_command "hello1"        ; short_sleep
 }
 
 : 'SEND A MESSAGE FROM CLIENT2 (1/2)' &&
 {
-    tmux_move_pain "${CLIENT2_PAIN}" ; short_sleep
-    tmux_run_command "hello2"        ; short_sleep
+  tmux_move_pain "${CLIENT2_PAIN}" ; short_sleep
+  tmux_run_command "hello2"        ; short_sleep
 }
 
 : 'SEND A MESSAGE FROM CLIENT1 (2/2)' &&
 {
-    tmux_move_pain "${CLIENT1_PAIN}" ; short_sleep
-    tmux_run_command "hello3"        ; short_sleep
+  tmux_move_pain "${CLIENT1_PAIN}" ; short_sleep
+  tmux_run_command "hello3"        ; short_sleep
 }
 
 : 'SEND A MESSAGE FROM CLIENT2 (2/2)' &&
 {
-    tmux_move_pain "${CLIENT2_PAIN}" ; short_sleep
-    tmux_run_command "hello4"        ; short_sleep
+  tmux_move_pain "${CLIENT2_PAIN}" ; short_sleep
+  tmux_run_command "hello4"        ; short_sleep
 }
