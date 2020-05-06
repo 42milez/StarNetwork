6810980708874a6dd54a63dec82dbc56613c37cc
Akihiro TAKASE <42milez@gmail.com>
2020-05-07 01:10:37 +0900

    add analyze service

---
+++ docker/script/analyze/perf.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DOCKER_SCRIPT_DIR=$(dirname $(dirname "${BASH_SOURCE}"))
+PERF='/usr/lib/linux-tools/4.15.0-99-generic/perf'
+TMP_DIR='/var/app/tmp'
+
+. "${DOCKER_SCRIPT_DIR}/config.bash"
+. "${DOCKER_SCRIPT_DIR}/var.bash"
+
+git clone https://github.com/brendangregg/FlameGraph /root/FlameGraph
+
+cd /root/FlameGraph
+
+cp "${BUILD_DIR}/bin/all_tests" .
+
+"${PERF}" record -F 99 -a -g ./all_tests
+"${PERF}" script | ./stackcollapse-perf.pl > "${TMP_DIR}/out.perf-folded"
+
+./flamegraph.pl "${TMP_DIR}/out.perf-folded" > "${TMP_DIR}/perf-kernel.svg"
